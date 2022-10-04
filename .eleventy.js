d8688a5daaffa02103b2ce067bcfd68c7cf14bdf
Glitch (first-end) <none>
2022-10-04 16:13:33 +0000

    🏈🗽 Checkpoint ./.eleventy.js:1433977/168

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 // docs: https://www.11ty.io/docs/config/
+const fs = require("fs");
 
 module.exports = function(eleventyConfig) {
   
@@ -7,7 +8,10 @@ module.exports = function(eleventyConfig) {
   // eleventyConfig.addFilter( "myFilter", function() {});
   eleventyConfig.on('eleventy.before', async ({ dir, runMode, outputMode }) => {
     // Read more below
-    
+    console.log("before", dir, runMode, outputMode)
+    fs.readdir("posts", (err,files) => {
+      console.log("files", files);
+    })
   });
   
   eleventyConfig.setBrowserSyncConfig({
